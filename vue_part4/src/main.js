6900ebf0011e82afba999f950e82d58299670d34
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2022-05-07 14:25:41 +0900

    vue-router設定

--- vue_part4/src/main.js
+++ vue_part4/src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import LikeNumber from './components/LikeNumber.vue'
+import router from "./router"
 
-Vue.config.productionTip = false
-Vue.component('LikeNumber', LikeNumber);
-Vue.filter("upperCase", function(value) {
-  return value.toUpperCase();
-});
+Vue.config.productionTip = false;
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
